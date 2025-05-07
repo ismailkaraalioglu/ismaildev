2b2e0d4ee1fc4912df0535902fcd77101dd6aaed
Ismail Karaalioglu <ismailkaraalioglu@yandex.com>
2025-05-07 16:51:52 +0300

    Update Divider component to be hidden on small screens and visible on larger screens

--- src/components/divider.tsx
+++ src/components/divider.tsx
@@ -1,6 +1,6 @@
 const Divider = () => {
   return (
-    <div className="min-h-screen w-10 border-x bg-[repeating-linear-gradient(315deg,var(--color-border),var(--color-border)_1px,transparent_1px,transparent_6px)]" />
+    <div className="hidden min-h-screen w-10 border-x bg-[repeating-linear-gradient(315deg,var(--color-border),var(--color-border)_1px,transparent_1px,transparent_6px)] sm:block" />
   );
 };
 
